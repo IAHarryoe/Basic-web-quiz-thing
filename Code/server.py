571baac8d51a3a1cf5d9bfa8437d40e7135e753f
IAHarryoe <isaac@henrikson.net>
2022-04-19 15:38:16 -0600

    Put stuff in folders
    Moved main files into folders

---
+++ Code/server.py
@@ -0,0 +1,78 @@
+import http.server
+import json
+import os
+
+
+#sets the active directory to the directory of the server.py file
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+quiz = open("Quiz.html", "r")
+quiz = quiz.read()
+totalAnswersCorrect = 0
+teamAnswersCorrect = {1:0,2:0}
+questionBankName = "questionBank.json"
+
+# creates a handler for the server that responds to requests with the html file
+class MyHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):#This is 
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        splitrq = self.requestline.split()
+        
+        questionBank = open("questionBank.json", "r")
+        quiz = open("Quiz.html", "r")
+        questions = questionBank.read()
+        quiz = quiz.read()
+        quiz = quiz.replace("[{THISTEXTISREPLACEDLATERBYSTUFF:'ok?'}]", questions)
+        self.wfile.write(bytes(quiz, "utf-8"))
+        #prints the message content
+        print("GET request received")
+        print("Request line:" + self.requestline)
+        print(f"")
+        
+        
+    def do_POST(self):#this is the function that handles POST requests sent from clients
+        global totalAnswersCorrect
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        quiz = open("Quiz.html", "r")
+        questionBank = open(questionBankName, "r")
+        questions = questionBank.read()
+        quiz = quiz.read()
+        quiz = quiz.replace("[{THISTEXTISREPLACEDLATERBYSTUFF:'ok?'}]", questions)
+        self.wfile.write(bytes(quiz, "utf-8"))
+        print("POST request received")
+        print("Request line:" + self.requestline)
+        #prints the message content
+        content_length = int(self.headers['Content-Length'])
+        messageData = str(self.rfile.read(content_length))
+        print(messageData)
+        if messageData.startswith("b'"):
+            messageData = messageData.replace("b'", "")
+        if messageData[-1] == "'":
+            messageData = messageData.removesuffix("'")
+        
+        
+        try:
+            formattedData = json.loads(messageData)
+            print(formattedData)
+            if formattedData["answerCorrect"]:
+                teamAnswersCorrect[formattedData["team"]] += 1
+                totalAnswersCorrect += 1
+                print(f"{totalAnswersCorrect} correct answers \n Team 1: {teamAnswersCorrect[1]} \n Team 2: {teamAnswersCorrect[2]}")
+        except:
+            pass
+        
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+# creates a server that listens on port 80
+server = http.server.HTTPServer(('', 80), MyHandler)
+
+# starts the server
+server.serve_forever()
+
